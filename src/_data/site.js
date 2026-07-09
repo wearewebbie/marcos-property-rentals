21f3d050393355d47f47d93aff53ee04a417f86e
wearewebbie <info@wearewebbie.com>
2026-07-09 09:11:28 +0800

    Initial commit

---
+++ src/_data/site.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Template",
+  description: "Website Description",
+  schemaType: "LocalBusiness",
+  url: process.env.CF_PAGES_BRANCH === 'main'
+    ? "https://PROD URL"
+    : process.env.CF_PAGES_URL || "http://localhost:8080",
+  logo: "",
+  image: "SEO Preview Image Card (1200 x 675 pixels)",
+  phone: "+44 000 000 000",
+  email: "info@example.com",
+  social: {
+    instagram: "https://instagram.com/example",
+    facebook: "https://facebook.com/example",
+    tiktok: ""
+  }
+};
